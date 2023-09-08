53ce1ec7b1abfacc8e590050a452ac5f4a70ab74
Gustavo Nogueira <gustavopereiranogueira@gmail.com>
2023-09-08 12:39:39 -0300

    feat: public route table

--- public.tf
+++ public.tf
@@ -28,3 +28,13 @@ resource "aws_subnet" "eks_subnet_public_1b" {
     }
   )
 }
+
+resource "aws_route_table_association" "eks_rtb_association_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
+
+resource "aws_route_table_association" "eks_rtb_association_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_public_route_table.id
+}
